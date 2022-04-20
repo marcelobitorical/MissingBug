fad82400a2e93b79d787a0f3b95980ba550c2bfe
Marcelo Arias <marias@bitorical.com>
2022-04-20 17:03:36 -0500

    White stripes book

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isMouseHolding, setIsMouseHolding] = useState(false);
+  const [deltaX, setDeltaX] = useState(0);
+
+  const handleTouchStart = (event) => {
+    setIsMouseHolding(true);
+  };
+
+  const handleTouchEnd = (event) => {
+    setIsMouseHolding(false);
+  }
+
+  const handleTouchMove = (event) => {
+    if (event.touches.length === 2) {
+      let delta_0 = (event.touches[0].clientX + event.touches[1].clientX ) / 2; 
+      // console.log({
+      //   "0x": event.touches[0].clientX,
+      //   "0y": event.touches[0].clientY,
+      //   "1y": event.touches[1].clientY,
+      //   "2y": event.touches[1].clientY,
+      // });
+      console.log({
+        delta_0
+      });
+      setDeltaX(delta_0);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        background: "skyblue",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <svg
+        className="box"
+        width="500"
+        height="500"
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        <g>
+          <foreignObject x="0" y="0" width="500" height="500">
+            <picture>
+              <img
+                alt=""
+                width="100%"
+                src="https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg"
+              />
+            </picture>
+          </foreignObject>
+        </g>
+      </svg>
+      <div>
+        <div style={{ width: deltaX, background: 'blue' }}></div>
+      </div>
     </div>
   );
 }
